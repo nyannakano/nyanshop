8868b3dcac27a131b64b7eb61f87d22728f135e5
nyannakano <gabrielnakano123@gmail.com>
2023-03-15 21:54:39 -0300

    Changed how the products are shown, and added an animation on mouse hover for products

--- src/Components/Catalog/index.js
+++ src/Components/Catalog/index.js
@@ -5,26 +5,30 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import Product from "../Product";
+import Boop from '../Animation/Boop'
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+const Item = styled(Paper)(({ theme }) => ({  // This const defines the style for the products
+    backgroundColor: '#a1def8',
     ...theme.typography.body2,
     padding: theme.spacing(2),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
 }));
 export default function Catalog() {
     return (
         <React.Fragment>
             <Container  fixed>
                 <Box sx={{
-                    flexGrow: 1}}>
-                    <Grid container spacing={{ xs: 1, md: 1}} columns={{ xs: 4, sm: 8, md: 12 }}>
-                        {Array.from(Array(6)).map((_, index) => (
+                    flexGrow: 1,
+                    backgroundColor: 'transparent'}}>
+                    <Grid container spacing={{ xs: 1, md: 1}} columns={{ xs: 4, sm: 8, md: 16 }}>
+                        {Array.from(Array(8)).map((_, index) => (
                             <Grid item xs={2} sm={4} md={4} key={index}>
+                                <Boop rotation={20} timing={200}>
                                 <Item>
                                     <Product />
+                                    <h1>{index+1}</h1>
                                 </Item>
+                                </Boop>
                             </Grid>
                         ))}
                     </Grid>
--- src/Components/Product/index.js
+++ src/Components/Product/index.js
@@ -3,7 +3,7 @@ import "./Product.css"
 export default function Product() {
     return (
         <React.Fragment>
-            <img src="http://github.com/nyannakano.png" alt=""/>
+            <img src="/images/favicon.png" alt=""/>
             <h2>Produto</h2>
         </React.Fragment>
     );
